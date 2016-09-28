70ad8466eb305149f8bb610e838861c379537b7f
Charneux Dimitri <charneux@b02p16.fil.univ-lille1.fr>
2016-09-28 11:57:14 +0200

    ex 1 et 2 fini

--- subdivision_Charneux/src/application/SubdivCurve.cpp
+++ subdivision_Charneux/src/application/SubdivCurve.cpp
@@ -12,56 +12,72 @@ SubdivCurve::~SubdivCurve() {
 }
 
 SubdivCurve::SubdivCurve() {
-  _nbIteration=1;
-  _source.clear();
-  _result.clear();
+    _nbIteration=1;
+    _source.clear();
+    _result.clear();
 
 }
 
 
 void SubdivCurve::addPoint(const p3d::Vector3 &p) {
-  _source.push_back(p);
+    _source.push_back(p);
 }
 
 void SubdivCurve::point(int i,const p3d::Vector3 &p) {
-  _source[i]=p;
+    _source[i]=p;
 }
 
 
 void SubdivCurve::chaikinIter(const vector<Vector3> &p) {
-  /* TODO : one iteration of Chaikin : input = p, output = you must set the vector _result (vector of Vector3)
-   */
-  _result.clear();
+    _result.clear();
+    if(isClosed())
+        _result.resize(2*p.size());
+    else
+        _result.resize(2*p.size()-2);
+    int i;
+    for(i = 0; i < p.size()-1; i++){
+        _result[2*i] = (3*p[i]/4) + (p[i+1]/4);
+        _result[2*i+1] = (p[i]/4) + (3*p[i+1]/4);
+    }
 
+    if(isClosed()){
+        _result[2*i] = (3*p[i]/4) + (p[0]/4);
+        _result[2*i+1] = (p[i]/4) + (3*p[0]/4);
+    }
 }
 
 void SubdivCurve::dynLevinIter(const vector<Vector3> &p) {
-  /* TODO : one iteration of DynLevin : input = p, output = you must set the vector _result (vector of Vector3)
-   */
-  _result.clear();
+    _result.clear();
+    _result.resize(2*p.size());
 
+    for(int i = 0; i < p.size(); i++){
+        _result[2*i] = p[i];
+        //attention au i-1 qu'il faut aussi moduler
+        _result[2*i+1] = (-( p[(i+2)%p.size()] + p[(p.size()+i-1)%p.size()] )/16)
+                + (9*(p[(i+1)%(p.size())] + p[i])/16);
+    }
 }
 
 
 void SubdivCurve::chaikin() {
-  if (_source.size()<2) return;
-  vector<Vector3> current;
-  _result=_source;
-  for(int i=0;i<_nbIteration;++i) {
-    current=_result;
-    chaikinIter(current);
-  }
+    if (_source.size()<2) return;
+    vector<Vector3> current;
+    _result=_source;
+    for(int i=0;i<_nbIteration;++i) {
+        current=_result;
+        chaikinIter(current);
+    }
 }
 
 void SubdivCurve::dynLevin() {
-  if (_source.size()<2) return;
-  if (!isClosed()) return;
-  vector<Vector3> current;
-  _result=_source;
-  for(int i=0;i<_nbIteration;++i) {
-    current=_result;
-    dynLevinIter(current);
-  }
+    if (_source.size()<2) return;
+    if (!isClosed()) return;
+    vector<Vector3> current;
+    _result=_source;
+    for(int i=0;i<_nbIteration;++i) {
+        current=_result;
+        dynLevinIter(current);
+    }
 }
 
 
